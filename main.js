b70131fa028f7b4d77d7f2eeea895e04717740e8
tavisca-vnagaraju <vnagaraju@tavisca.in>
2019-08-21 00:25:14 +0530

    added edit/delete form validation using js

---
+++ main.js
@@ -0,0 +1,92 @@
+function AddToDo()
+{
+    var todo = document.getElementById("to-do-name");
+    if(todo.value)
+    {
+        var table = document.getElementById("to-do-table-id");
+        var tr = document.createElement("tr");
+        var tdDate = document.createElement("td");
+        var tdName = document.createElement("td");
+        var tdEdit = document.createElement("td");
+        var editButton = document.createElement("button");
+        var deleteButton = document.createElement("button");
+        
+        var editButtonText = document.createTextNode("Edit");
+        editButton.appendChild(editButtonText);
+        editButton.classList.add("edit-button");
+        editButton.setAttribute("onclick","showTextBox(this)");
+        
+        var deleteButtonText = document.createTextNode("Delete");
+        deleteButton.appendChild(deleteButtonText);
+        deleteButton.classList.add("delete-button");
+        deleteButton.setAttribute("onclick","deleteElement(this)");
+        
+        
+        var date = new Date();
+        var dateNode = document.createTextNode(date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear());
+        var name = document.createTextNode(todo.value);
+    
+        tdDate.appendChild(dateNode);
+        tdName.appendChild(name);
+        tdEdit.appendChild(editButton);
+        tdEdit.appendChild(deleteButton);
+    
+        tr.appendChild(tdDate);
+        tr.appendChild(tdName);
+        tr.appendChild(tdEdit);
+    
+        table.appendChild(tr);
+    }
+}
+function deleteElement(element){
+    element.parentElement.parentElement.remove();
+}
+function getInputTextBox(){
+    var inputBox = document.createElement("input");
+    inputBox.setAttribute("type","text");
+    return inputBox;
+}
+function showTextBox(element){
+    var edit = element.parentElement;
+    var todoName = element.parentElement.previousElementSibling;
+    var inputBox = getInputTextBox();
+    inputBox.setAttribute("value",todoName.innerHTML);
+    todoName.replaceChild(inputBox,todoName.childNodes[0]);
+    
+    var saveButton = document.createElement("button");
+    var textNode = document.createTextNode("Save");
+    saveButton.classList.add("edit-button");
+    saveButton.setAttribute("onclick","saveText(this)");
+    saveButton.appendChild(textNode);
+
+    edit.replaceChild(saveButton,edit.childNodes[0]);
+
+}
+function saveText(element){
+    var inputTextNode = element.parentElement.previousElementSibling;
+    var inputText = inputTextNode.childNodes[0].value;
+    
+    inputTextNode.innerHTML = inputText;
+    element.setAttribute("onclick","showTextBox(this)");
+    element.innerHTML = "Edit";
+}
+function validateForm() {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var passwordformat= /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+    var username = document.forms["login-form"]["username"].value;
+    var password = document.forms["login-form"]["password"].value;
+    if (username == "") {
+      alert("Mail must not be empty");
+      return false;
+    }
+    else if(!username.match(mailformat)){
+        alert("unknown mail format");
+        return false;
+    }
+    else if(password == ""){
+        alert("Please enter some password");
+    }
+    else if(!password.match(passwordformat)){
+        alert("Weak Password , Password Must Contain atleast one Capital letter, one Small letter, one digit,one special character, length must be between 7 to 15 characters");
+    }
+  }
